4735c83d3eb81303eb6a5cf7782bd5dd961fc112
jordan38 <jordan.content@outlook.fr>
2017-09-14 13:09:45 +0200

    Feed and play feature

--- TamagotchiKataTest/TamagotchiShould.cs
+++ TamagotchiKataTest/TamagotchiShould.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,54 @@ namespace TamagotchiKataTest
         [Test]
         public void BeMoreFullnessAndHaveLessStarvingWhenWeFeedHim()
         {
+            var tamagotchi = new Tamagotchi();
+            var increase = 6;
+            var decrease = 4;
 
+            tamagotchi.Feed();
+
+            increase.Should().BeGreaterOrEqualTo(tamagotchi.Fullness);
+            decrease.Should().BeLessOrEqualTo(tamagotchi.Hungriness);
+        }
+
+        [Test]
+        public void BeMoreTiredAndMoreHappierWhenWePlayWithHim()
+        {
+            var tamagotchi = new Tamagotchi();
+            var increase = 6;
+
+            tamagotchi.Play();
+
+            increase.Should().BeGreaterOrEqualTo(tamagotchi.Happiness);
+            increase.Should().BeGreaterOrEqualTo(tamagotchi.Tiredness);
+        }
+    }
+
+    public class Tamagotchi
+    {
+        public int Hungriness;
+        public int Fullness;
+        public int Happiness;
+        public int Tiredness;
+
+        public Tamagotchi()
+        {
+            Hungriness = 5;
+            Fullness = 5;
+            Happiness = 5;
+            Tiredness = 5;
+        }
+
+        public void Feed()
+        {
+            Hungriness -= 1;
+            Fullness += 1;
+        }
+
+        internal void Play()
+        {
+            Happiness += 1;
+            Tiredness += 1;
         }
     }
 }
